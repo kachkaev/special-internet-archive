4e5c59a19bc88a4c7f40020cf6298b19468534de
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-08 16:07:28 +0100

    Upgrade envalid to 7.3.1 and simplify cleanEnv

--- shared/clean-env.ts
+++ shared/clean-env.ts
@@ -3,26 +3,19 @@ import * as envalid from "envalid";
 
 import { UserFriendlyError } from "./user-friendly-error";
 
-export const customEnvalidReporter: typeof envalid.defaultReporter = (
-  errors,
-) => {
-  const outputChunks: string[] = [];
-  envalid.defaultReporter(errors, {
-    logger: (message) => {
-      outputChunks.push(message);
-    },
-    onError: () => {
-      throw new UserFriendlyError(outputChunks.join("\n"));
-    },
-  });
-};
-
 export const cleanEnv = <T>(specs: {
   [K in keyof T]: envalid.ValidatorSpec<T[K]>;
 }) => {
   config({ silent: true });
 
   return envalid.cleanEnv(process.env, specs, {
-    reporter: customEnvalidReporter,
+    reporter: ({ errors }) => {
+      if (Object.keys(errors).length === 0) {
+        return;
+      }
+
+      envalid.envalidErrorFormatter(errors);
+      throw new UserFriendlyError();
+    },
   });
 };
--- shared/user-friendly-error.ts
+++ shared/user-friendly-error.ts
@@ -5,8 +5,10 @@ export class UserFriendlyError extends Error {}
 if (typeof process !== "undefined") {
   process.on("uncaughtException", (error) => {
     if (error instanceof UserFriendlyError) {
-      // eslint-disable-next-line no-console
-      console.log(chalk.red(error.message));
+      if (error.message) {
+        // eslint-disable-next-line no-console
+        console.log(chalk.red(error.message));
+      }
     } else {
       // eslint-disable-next-line no-console
       console.log(error);
