cf2a8a1870c1e89949e5fda5491b053c48ff0e59
Alexander Kachkaev <alexander@kachkaev.ru>
2022-04-09 19:41:33 +0100

    Implement processWebPages

--- scripts/web-pages/wayback-machine/1-fetch-snapshot-infos.script.ts
+++ scripts/web-pages/wayback-machine/1-fetch-snapshot-infos.script.ts
@@ -1,24 +1,17 @@
 import axios from "axios";
 import chalk from "chalk";
-import fs from "fs-extra";
 import _ from "lodash";
 import { DateTime } from "luxon";
 import sortKeys from "sort-keys";
 
-import {
-  getWebPagesDirPath,
-  relevantTimeMin,
-} from "../../../shared/collection";
+import { relevantTimeMin } from "../../../shared/collection";
 import { serializeTime } from "../../../shared/helpers-for-json";
-import { processFiles } from "../../../shared/process-files";
+import { processWebPages } from "../../../shared/process-web-pages";
 import {
   listWebPageAliases,
   writeWebPageDocument,
 } from "../../../shared/web-pages";
-import {
-  WaybackMachineAliasInfo,
-  WebPageDocument,
-} from "../../../shared/web-pages/types";
+import { WaybackMachineAliasInfo } from "../../../shared/web-pages/types";
 
 const output = process.stdout;
 
@@ -36,102 +29,122 @@ const expectedColumnsInCdxApiResponse: CdxApiResponse[number] = [
   "length",
 ];
 
+const fetchSnapshotTimes = async (url: string): Promise<string[]> => {
+  const result: string[] = [];
+  // E.g. http://web.archive.org/cdx/search/cdx?url=https://vk.com/penza_live&output=json
+  const { data: rawCdxApiResponse } = await axios.get<CdxApiResponse>(
+    "https://web.archive.org/cdx/search/cdx",
+    {
+      responseType: "json",
+      transitional: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention -- external API
+        silentJSONParsing: false, // Disables Object to string conversion if parsing fails
+      },
+      params: { url, output: "json" },
+    },
+  );
+
+  const [csvHeaderCells, ...csvRows] = rawCdxApiResponse;
+
+  if (
+    csvRows.length > 0 &&
+    !_.isEqual(expectedColumnsInCdxApiResponse, csvHeaderCells)
+  ) {
+    throw new Error(
+      "Unexpected columns in CDX API response. API signature must have changed, scripts need updating.",
+    );
+  }
+
+  for (const cells of csvRows) {
+    const statusCode = cells[4];
+
+    if (statusCode !== "200" && statusCode !== "404") {
+      continue;
+    }
+
+    const timestamp = cells[1];
+    const serializedTime = serializeTime(timestamp);
+    if (serializedTime < relevantTimeMin) {
+      continue;
+    }
+
+    result.push(serializedTime);
+  }
+
+  return result;
+};
+
 const script = async () => {
   output.write(chalk.bold("Fetching Wayback Machine snapshot infos\n"));
 
-  await processFiles({
-    fileSearchPattern: "**/*.(json)",
-    fileSearchDirPath: getWebPagesDirPath(),
-    filesNicknameToLog: "web pages",
+  await processWebPages({
     output,
-    processFile: async (webPageDocumentFilePath, prefixLength) => {
-      const webPageDocument = (await fs.readJson(
-        webPageDocumentFilePath,
-      )) as WebPageDocument;
-
+    processWebPage: async (webPageDocument) => {
+      const updatedWebPageDocument = _.cloneDeep(webPageDocument);
       const aliasUrls = listWebPageAliases(webPageDocument.url);
 
-      webPageDocument.waybackMachine ??= {};
-      webPageDocument.waybackMachine.snapshotInfoByAlias ??= {};
-
-      output.write(" ".repeat(prefixLength));
+      updatedWebPageDocument.waybackMachine ??= {};
+      updatedWebPageDocument.waybackMachine.snapshotInfoByAlias ??= {};
 
       const minSerializedTimeToRefetch = serializeTime(
-        DateTime.utc().minus({ minutes: 5 }),
+        DateTime.utc().minus({ minutes: 1 }),
       );
+
       for (const aliasUrl of aliasUrls) {
-        const existingSerializedTime =
-          webPageDocument.waybackMachine.snapshotInfoByAlias[aliasUrl]
-            ?.fetchedAt;
+        const existingAliasInfo =
+          webPageDocument.waybackMachine.snapshotInfoByAlias?.[aliasUrl];
+        output.write(chalk.green(`\n  alias ${aliasUrl} `));
 
         if (
-          existingSerializedTime &&
-          existingSerializedTime > minSerializedTimeToRefetch
+          existingAliasInfo &&
+          existingAliasInfo.fetchedAt > minSerializedTimeToRefetch
         ) {
-          output.write(` ${aliasUrl} [-]  `);
+          output.write(
+            chalk.gray(
+              `fetching skipped; snapshot count: ${existingAliasInfo.snapshotTimes.length}`,
+            ),
+          );
           continue;
         }
 
         try {
           const aliasInfo: WaybackMachineAliasInfo = {
             fetchedAt: serializeTime(),
-            snapshotTimes: [],
+            snapshotTimes: await fetchSnapshotTimes(aliasUrl),
           };
 
-          // E.g. http://web.archive.org/cdx/search/cdx?url=https://vk.com/penza_live&output=json
-          const { data: rawCdxApiResponse } = await axios.get<CdxApiResponse>(
-            "https://web.archive.org/cdx/search/cdx",
-            {
-              responseType: "json",
-              transitional: {
-                // eslint-disable-next-line @typescript-eslint/naming-convention -- external API
-                silentJSONParsing: false, // Disables Object to string conversion if parsing fails
-              },
-              params: { url: aliasUrl, output: "json" },
-            },
-          );
-
-          const [csvHeaderCells, ...csvRows] = rawCdxApiResponse;
-
-          if (
-            csvRows.length > 0 &&
-            !_.isEqual(expectedColumnsInCdxApiResponse, csvHeaderCells)
-          ) {
-            throw new Error(
-              "Unexpected columns in CDX API response. API signature must have changed, scripts need updating.",
-            );
-          }
-
-          for (const cells of csvRows) {
-            const statusCode = cells[4];
-
-            if (statusCode !== "200" && statusCode !== "404") {
-              continue;
-            }
-
-            const timestamp = cells[1];
-            const serializedTime = serializeTime(timestamp);
-            if (serializedTime < relevantTimeMin) {
-              continue;
-            }
-
-            aliasInfo.snapshotTimes.push(serializedTime);
-          }
-
-          webPageDocument.waybackMachine.snapshotInfoByAlias[aliasUrl] =
+          updatedWebPageDocument.waybackMachine.snapshotInfoByAlias[aliasUrl] =
             aliasInfo;
 
-          output.write(` ${aliasUrl} [${aliasInfo.snapshotTimes.length}]  `);
+          if (
+            _.isEqual(aliasInfo.snapshotTimes, existingAliasInfo?.snapshotTimes)
+          ) {
+            output.write(
+              chalk.blue(
+                `fetched; snapshot count: ${aliasInfo.snapshotTimes.length} (did not change)`,
+              ),
+            );
+          } else {
+            output.write(
+              chalk.magenta(
+                `fetched; snapshot count: ${
+                  aliasInfo.snapshotTimes.length
+                } (changed from ${
+                  existingAliasInfo?.snapshotTimes.length ?? 0
+                })`,
+              ),
+            );
+          }
         } catch (error) {
-          output.write(` ${aliasUrl} ${error as string}  `);
+          output.write(`${chalk.red(error)}  `);
         }
       }
 
-      webPageDocument.waybackMachine.snapshotInfoByAlias = sortKeys(
-        webPageDocument.waybackMachine.snapshotInfoByAlias,
+      updatedWebPageDocument.waybackMachine.snapshotInfoByAlias = sortKeys(
+        updatedWebPageDocument.waybackMachine.snapshotInfoByAlias,
       );
 
-      await writeWebPageDocument(webPageDocument);
+      await writeWebPageDocument(updatedWebPageDocument);
       output.write("\n");
     },
   });
---
+++ shared/process-web-pages.ts
@@ -0,0 +1,69 @@
+import chalk from "chalk";
+import * as envalid from "envalid";
+import fs from "fs-extra";
+import { WriteStream } from "node:tty";
+import RegexParser from "regex-parser";
+
+import { cleanEnv } from "./clean-env";
+import { getWebPagesDirPath } from "./collection";
+import { listFilePaths } from "./list-file-paths";
+import { WebPageDocument } from "./web-pages/types";
+
+export const processWebPages = async ({
+  output,
+  processWebPage,
+}: {
+  output?: WriteStream | undefined;
+  processWebPage: (webPageDocument: WebPageDocument) => void | Promise<void>;
+}) => {
+  const env = cleanEnv({
+    FILTER_URL: envalid.str({
+      desc: "Glob to use when filtering URLs",
+      default: ".*",
+    }),
+  });
+  const filterUrlRegex = RegexParser(env.FILTER_URL);
+
+  let numberOfDone = 0;
+  let numberOfErrors = 0;
+  let numberOfSkipped = 0;
+
+  const webPageDocumentFilePaths = await listFilePaths({
+    filesNicknameToLog: "web pages",
+    fileSearchPattern: "**/web-page.json",
+    fileSearchDirPath: getWebPagesDirPath(),
+    output,
+  });
+
+  output?.write(chalk.green("Processing web pages..."));
+
+  for (const webPageDocumentFilePath of webPageDocumentFilePaths) {
+    const webPageDocument = (await fs.readJson(
+      webPageDocumentFilePath,
+    )) as WebPageDocument;
+
+    output?.write(`\n${chalk.underline(webPageDocument.url)}`);
+
+    if (!filterUrlRegex.test(webPageDocument.url)) {
+      numberOfSkipped += 1;
+      output?.write(chalk.gray(` does not match FILTER_URL`));
+
+      continue;
+    }
+
+    try {
+      await processWebPage(webPageDocument);
+      numberOfDone += 1;
+    } catch (error) {
+      numberOfErrors += 1;
+      output?.write(chalk.red(`Unexpected error ${error as string}`));
+      // throw error;
+    }
+  }
+
+  output?.write(
+    `\n\nWeb pages in collection: ${
+      numberOfDone + numberOfErrors + numberOfSkipped
+    } (processed: ${numberOfDone}, errors: ${numberOfErrors}, filtered out: ${numberOfSkipped})\n`,
+  );
+};
