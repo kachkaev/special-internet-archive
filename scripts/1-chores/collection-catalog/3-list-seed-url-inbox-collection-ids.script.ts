a5945da2de2df4c106ba6c5876a9cc6659f68a96
Alexander Kachkaev <alexander@kachkaev.ru>
2022-05-15 02:18:36 +0100

    Implement FILTER_COLLECTION_ID

--- scripts/1-chores/collection-catalog/3-list-seed-url-inbox-collection-ids.script.ts
+++ scripts/1-chores/collection-catalog/3-list-seed-url-inbox-collection-ids.script.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import * as envalid from "envalid";
 import { globby } from "globby";
 import _ from "lodash";
+import RegexParser from "regex-parser";
 
 import { cleanEnv } from "../../../shared/clean-env";
 import { getCollectionCatalogSeedUrlInboxesDirPath } from "../../../shared/collection-catalog";
@@ -10,12 +11,17 @@ const output = process.stdout;
 
 const script = async () => {
   const env = cleanEnv({
+    FILTER_COLLECTION_ID: envalid.str({
+      desc: "Regex to use when collection ids",
+      default: ".*",
+    }),
     OUTPUT_FORMAT: envalid.str({
       choices: ["raw", "json"],
       default: "raw",
     }),
   });
   const outputFormat = env.OUTPUT_FORMAT;
+  const filterCollectionIdRegex = RegexParser(env.FILTER_COLLECTION_ID);
 
   if (outputFormat !== "json") {
     output.write(chalk.bold(`Listing seed URL inbox collection IDs\n`));
@@ -28,12 +34,18 @@ const script = async () => {
   const collectionIds = _.orderBy(
     fileNames.map((fileName) => fileName.slice(0, -4)),
   );
-
+  const filteredCollectionIds = collectionIds.filter((collectionId) =>
+    collectionId.match(filterCollectionIdRegex),
+  );
   if (outputFormat === "json") {
-    output.write(`${JSON.stringify(collectionIds)}\n`);
+    output.write(`${JSON.stringify(filteredCollectionIds)}\n`);
   } else {
-    output.write(`Collections found: ${collectionIds.length}\n`);
-    for (const collectionId of collectionIds) {
+    output.write(`Collections found: ${collectionIds.length}`);
+    if (filteredCollectionIds.length !== collectionIds.length) {
+      output.write(`, filtered: ${filteredCollectionIds.length}`);
+    }
+    output.write("\n");
+    for (const collectionId of filteredCollectionIds) {
       output.write(`- ${chalk.blue(collectionId)}\n`);
     }
   }
