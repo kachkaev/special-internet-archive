4255ee2ac5424909aa740ffcef34f2533a176641
Alexander Kachkaev <alexander@kachkaev.ru>
2022-06-11 23:46:21 +0100

    Make FILTER_COLLECTION_ID "" by default

--- scripts/1-chores/collection-catalog/3-list-seed-url-inbox-collection-ids.script.ts
+++ scripts/1-chores/collection-catalog/3-list-seed-url-inbox-collection-ids.script.ts
@@ -13,7 +13,7 @@ const script = async () => {
   const env = cleanEnv({
     FILTER_COLLECTION_ID: envalid.str({
       desc: "Regex to filter collections",
-      default: ".*",
+      default: "",
     }),
     OUTPUT_FORMAT: envalid.str({
       choices: ["raw", "json"],
@@ -21,7 +21,9 @@ const script = async () => {
     }),
   });
   const outputFormat = env.OUTPUT_FORMAT;
-  const filterCollectionIdRegex = RegexParser(env.FILTER_COLLECTION_ID);
+  const filterCollectionIdRegex = env.FILTER_COLLECTION_ID
+    ? RegexParser(env.FILTER_COLLECTION_ID)
+    : undefined;
 
   if (outputFormat !== "json") {
     output.write(chalk.bold(`Listing seed URL inbox collection IDs\n`));
@@ -34,8 +36,9 @@ const script = async () => {
   const collectionIds = _.orderBy(
     fileNames.map((fileName) => fileName.slice(0, -4)),
   );
-  const filteredCollectionIds = collectionIds.filter((collectionId) =>
-    collectionId.match(filterCollectionIdRegex),
+  const filteredCollectionIds = collectionIds.filter(
+    (collectionId) =>
+      !filterCollectionIdRegex || collectionId.match(filterCollectionIdRegex),
   );
   if (outputFormat === "json") {
     output.write(`${JSON.stringify(filteredCollectionIds)}\n`);
