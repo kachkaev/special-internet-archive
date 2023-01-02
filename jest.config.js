0ab1c1d6e8ea30fa71d799ec227e346cdca6b877
Alexander Kachkaev <alexander@kachkaev.ru>
2023-01-02 16:39:54 +0100

    Fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   preset: "ts-jest/presets/js-with-babel-esm",
+  // @todo Remove when https://github.com/facebook/jest/pull/13705 is released
+  moduleNameMapper: {
+    /* eslint-disable @typescript-eslint/naming-convention */
+    "#ansi-styles": "ansi-styles/index.js",
+    "#supports-color": "supports-color/index.js",
+    /* eslint-enable @typescript-eslint/naming-convention */
+  },
 };
